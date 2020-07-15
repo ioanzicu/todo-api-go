9cd432f52ba8b55f5e7399e2bef2cd22fefa4dd3
Ioan <ioan.zicu94@gmail.com>
2020-07-15 14:47:53 +0000

    move comment

--- controller/crud.go
+++ controller/crud.go
@@ -11,6 +11,7 @@ import (
 func create() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
+			// curl -H 'Content-Type: application/json' --request POST -d '{"name":"Kratos", "todo":"Kill the Zeus"}' localhost:3000/create
 			data := views.PostRequest{}
 			json.NewDecoder(r.Body).Decode(&data)
 			fmt.Println(data)
@@ -29,6 +30,7 @@ func create() http.HandlerFunc {
 			w.WriteHeader(http.StatusOK)
 			json.NewEncoder(w).Encode(data)
 		} else if r.Method == http.MethodDelete {
+			// curl -X DELETE localhost:3000/Arto
 			var name string = r.URL.Path[1:]
 			if err := model.DeleteTodo(name); err != nil {
 				w.Write([]byte("Some error - Delete"))
--- controller/route.go
+++ controller/route.go
@@ -7,8 +7,6 @@ import (
 func Register() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", ping())
-	// EXAMPLE: 
-	// curl -H 'Content-Type: application/json' --request POST -d '{"name":"Kratos", "todo":"Kill the Zeus"}' localhost:3000/create
 	mux.HandleFunc("/", create())
 	return mux
 }

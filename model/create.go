076938f1c5244e5944a20116281e40d3152930b3
Ioan <ioan.zicu94@gmail.com>
2020-07-15 09:56:36 +0000

    Create todo via POST request and decode it from JSON format

--- model/create.go
+++ model/create.go
@@ -2,8 +2,8 @@ package model
 
 import "fmt"
 
-func CreateTodo() error {
-	insertQ, err := con.Query("INSERT INTO TODO VALUES(?, ?)", "Ioan", "Go to sleep at 22:00")
+func CreateTodo(name, todo string) error {
+	insertQ, err := con.Query("INSERT INTO TODO VALUES(?, ?)", name, todo)
 	defer insertQ.Close()
 
 	if err != nil {

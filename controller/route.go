04fdd654e3fc076e16736a19601c6200e7391f16
Ioan <ioan.zicu94@gmail.com>
2020-07-15 10:13:15 +0000

    read all todos

--- controller/route.go
+++ controller/route.go
@@ -9,6 +9,6 @@ func Register() *http.ServeMux {
 	mux.HandleFunc("/ping", ping())
 	// EXAMPLE: 
 	// curl -H 'Content-Type: application/json' --request POST -d '{"name":"Kratos", "todo":"Kill the Zeus"}' localhost:3000/create
-	mux.HandleFunc("/create", create())
+	mux.HandleFunc("/", create())
 	return mux
 }
---
+++ model/reads.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"../views"
+)
+
+func ReadAll() ([]views.PostRequest, error) {
+	rows, err := con.Query("SELECT * FROM TODO")
+	if err != nil {
+		return nil, err
+	}
+
+	todos := []views.PostRequest{}
+	for rows.Next() {
+		data := views.PostRequest{}
+		rows.Scan(&data.Name, &data.Todo)
+		todos = append(todos, data)
+	}
+
+	return todos, nil
+}
